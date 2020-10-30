a58552d7bd1ba0a1dcc13675cba24f099a43b9c3
alfredojl <alfredo.jl323@gmail.com>
2020-10-30 07:20:26 -0600

    archivos agregados

---
+++ back/helpers/traeFaltantes.js
@@ -0,0 +1,32 @@
+const Paquete = require('../server/models/Paquetes');
+
+const mongoose = require('mongoose');
+
+
+require('../server/config/config');
+
+mongoose.connect('mongodb://localhost:27017/registro', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, (err, res) => {
+    if (err) throw err;
+    console.log("BD ONLINE");
+});
+
+const getFaltantes = () => {
+    let faltantes = null;
+    Paquete.find({ folioInicio: null }, (err, faltantesDB) => {
+        if (err)
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        res.json({
+            ok: true,
+            faltantes = faltantesDB
+        });
+    })
+
+}
---
+++ front/src/views/Reporte.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <b-row>
+            <b-col md="auto">
+      <b-calendar v-model="value" @context="onContext" locale="es-MX"></b-calendar>
+    </b-col>
+        </b-row>
+    </div>
+</template>
