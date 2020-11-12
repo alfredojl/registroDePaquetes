36ad679149bc6bd28a018f1a39554baafe161148
alfredojl <alfredo.jl323@gmail.com>
2020-11-12 09:05:17 -0600

    Incidencia

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -470,6 +470,7 @@ export default {
             }
           });
           if (this.faltantes.length > 0) this.incidencia = true;
+          else incidencia = false;
         })
         .catch((error) => {
           if (error) console.log(error);
