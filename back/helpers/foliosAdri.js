7e41640223b9777b085bfe292433d173c526d102
alfredojl <alfredo.jl323@gmail.com>
2021-08-06 10:04:16 -0500

    foliosAdri.js for Sice list verification.

--- back/helpers/foliosAdri.js
+++ back/helpers/foliosAdri.js
@@ -14,13 +14,9 @@ const checkFolios = async () => {
 
   // let lista = require("./foliosAdri.json");
   let resultado = [];
-  const lista = xlsx.readFile("./pedirVictorVersionFinalV2.xlsx");
+  const lista = xlsx.readFile("./revisarSICE.csv");
   let heads = lista.SheetNames;
-  let temp = xlsx.utils.sheet_to_json(lista.Sheets[heads[0]], {
-    skipHeader: true,
-  });
-
-  //   lista = lista.slice(0, 20);
+  let temp = xlsx.utils.sheet_to_json(lista.Sheets[heads[0]]);
   try {
     //   try {
     //     fs.unlinkSync("./reporteFoliosAdri.csv");
@@ -39,43 +35,63 @@ const checkFolios = async () => {
     //   }
     let total = temp.length;
     let index = 0;
+    let status = '';
+    // ? Checa en SICE la existencia de los folios
     for (el of temp) {
-      let archivo = el.PDF.split('.')
-      let partes = archivo[0].split("_");
-      let folio = parseInt(partes[0]);
-      let tomo = partes[1] ? "= " + partes[1] : "IS NULL";
-      let t = partes[1] ? partes[1] : "";
-      process.stdout.write(
-        `${folio}\t\t${tomo} ${t}\t\t${++index} de ${total}`
-      );
-      let consult = await res.query(
-        `SELECT C24 Paquete, C22 Folio, C25 Tomo, C11242 "# Imágenes" FROM T15 WHERE Usuario = "DEVELOPMENT S&H" AND C22 = ${folio} AND C25 ${tomo};`
-      );
-      if (!!consult && consult.length > 0) {
-        process.stdout.write(" ¡Sí!\n".green);
-        resultado.push({
-          folio,
-          tomo: t,
-          status: "Encontrado",
-        });
-      } else {
-        process.stdout.write(" ¡No!\n".red);
-        resultado.push({
-          folio,
-          tomo: t,
-          status: "No encontrado",
-        });
+      process.stdout.write(`${++index}/${total}\t${el.Folio} ${el.Tomo ? el.Tomo : ''}\t`.yellow);
+      let consulta = await res.query(`SELECT Id FROM T15 WHERE Usuario = "DEVELOPMENT S&H" AND C22 = ${el.Folio} AND C25 ${el.Tomo ? '= ' + el.Tomo : 'IS NULL'}`);
+      if (consulta.length > 0) {
+        process.stdout.write(`Sí\n`.green)
+        status = true;
       }
+      else {
+        process.stdout.write(`No\n`.cyan)
+        status = false;
+      }
+      resultado.push({
+        ...el,
+        status
+      });
     }
+    // ? Fin del checa.    
+    // ! Creo que creaba en la BD.
+    //* for (el of temp) {
+    //*   let archivo = el.PDF.slice(0, el.PDF.indexOf('.'));
+    //*   let partes = archivo.split("_");
+    //*   let folio = parseInt(partes[0]);
+    //*   let tomo = partes.length == 3 ? "= " + partes[1] : "IS NULL";
+    //*   let t = partes.length == 3 ? partes[1] : "";
+    //*   process.stdout.write(
+    //*     `${folio}\t\t${tomo} ${t}\t\t${++index}/${total}`
+    //*   );
+    //*   let consult = await res.query(
+    //*     `SELECT C24 Paquete, C22 Folio, C25 Tomo, C11242 "# Imágenes" FROM T15 WHERE Usuario = "DEVELOPMENT S&H" AND C22 = ${folio} AND C25 ${tomo};`
+    //*   );
+    //*   if (!!consult && consult.length > 0) {
+    //*     process.stdout.write(" ¡Sí!\n".green);
+    //*     resultado.push({
+    //*       PDF: el.PDF,
+    //*       status: "Encontrado",
+    //*     });
+    //*   } else {
+    //*     process.stdout.write(" ¡No!\n".red);
+    //*     resultado.push({
+    //*       PDF: el.PDF,
+    //*       status: "No encontrado",
+    //*     });
+    //*   }
+    //* }
+    // ! Fin del creo.
     let doc, libro;
     doc = xlsx.utils.json_to_sheet(resultado);
     libro = xlsx.utils.book_new();
-    xlsx.utils.book_append_sheet(libro, doc, 'reporteVicSICE');
-    xlsx.writeFile(libro, `./reporteVicSICE.xlsx`);
-    console.log(`[reporteVicSICE.xlsx] created.`);
+    xlsx.utils.book_append_sheet(libro, doc);
+    xlsx.writeFile(libro, `./auditoriaSICEyServidor.xlsx`);
+    console.log(`[ auditoriaSICEyServidor.xlsx ] created.`);
     process.exit();
   } catch (error) {
     console.log(error);
+    process.exit();
   }
 };
 
